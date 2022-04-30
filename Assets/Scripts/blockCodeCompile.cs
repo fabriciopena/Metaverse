4e186f30d371df93a6eb6bea8c4e8102e29d8d1f
GuyWhoCode <thatguywhocodes@gmail.com>
2022-04-29 19:27:27 -0700

    Finished Block Compiling script

--- Assets/Scripts/blockCodeCompile.cs
+++ Assets/Scripts/blockCodeCompile.cs
@@ -8,20 +8,11 @@ public class blockCodeCompile : MonoBehaviour {
         for (int childCounter = 1; childCounter < startBlock.childCount; childCounter ++) {
             Transform currentBlockRun = startBlock.GetChild(childCounter).GetChild(0);
             MonoBehaviour blockScriptName = currentBlockRun.GetComponent<MonoBehaviour>();
-            // Code came from https://answers.unity.com/questions/62658/how-to-get-all-scripts-attached-to-a-gameojbect.html
 
-            Debug.Log(blockScriptName);
-            // Expected behavior: supposed to be able to call the public function runCode() within each of these scripts
-            // Scripts being called: printFavoriteItem (attached on the sky blue sphere)           
-            // printName (reddish sphere)
-            // printRandomNumber (blue sphere)
-
-
-            // var blockScript = currentBlockRun.GetComponent<blockScriptName>();
-            // Spaghetti code I tried to use to try to solve it
-
+            blockScriptName.BroadcastMessage("runCode");
+            // Calls the inner function of the script using a built-in method of MonoBehaviour
+            // When making new Block Scripts, the compiler will look for the following function:
+            // public void runCode() {}
         }
-        // Debug.Log("RunBlockScript".Contains("BlockScript"));
-        
     }
 }
